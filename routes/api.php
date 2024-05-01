97271624190558e3eae813c3462769a70bc807f0
Jertroll <jeremyblanco20@gmail.com>
2024-05-01 01:44:25 -0600

    CRUD de api iniciando

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,39 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
+/** Route::get('/user', function (Request $request) {
     return $request->user();
-})->middleware('auth:sanctum');
+})->middleware('auth:sanctum'); */
+
+Route::get('/User', function(){
+return 'Obteniendo lista de usuarios';
+});
+
+Route::get('/User/{id}', function(){
+return 'Obteniendo un usuario';
+ });
+   
+Route::post('/User', function(){
+ return 'Creando un usuario';
+});
+
+Route::put('/User/{id}', function(){
+ return 'Actualizando un usuario';
+ });
+
+ Route::delete('/User/{id}', function(){
+return 'Eliminando un usuario';
+});
+
+
+
+
+Route::prefix('v1')->group(
+function(){
+    //rutas especificas
+
+    //rutas automaticas Restful
+    
+}
+
+);
