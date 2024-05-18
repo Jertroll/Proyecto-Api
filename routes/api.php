e129292413384907c165685725d454637d9707dd
Kristhel04 <149755958+Kristhel04@users.noreply.github.com>
2024-05-17 18:30:33 -0600

    rutas api actualizado funcional

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,40 @@
 <?php
 
+
+
+
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\BillController;
-use App\Http\Controllers\CarritoController;
+
 use App\Http\Controllers\ProductoController;
 use App\Http\Controllers\CompraController;
 use App\Http\Controllers\ProductoCarritoController;
 use App\Http\Controllers\DetalleFacturaController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Middleware\ApiAuthMiddleware;
+use App\Http\Middleware\ApiAuthMiddleware; 
+use App\Http\Controllers\CarritoController;
 use App\Http\Middleware\AdminMiddleware;
 use App\Http\Middleware\UserMiddleware;
 
 Route::prefix('v1')->group(function () {
+  
+      //rutas automaticas Restful Admin
+      Route::group(['prefix' => '/admin'], function () {
+       
+        Route::get('/user/getidentity', [UserController::class, 'getIdentity'])->middleware(ApiAuthMiddleware::class);
+
+        Route::resource('/compra', CompraController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
+        Route::resource('/user', UserController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]); //Se excluyen porque son obsolutas por temas de seguridad 
+        Route::resource('/bill', BillController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
+        Route::resource('/producto', ProductoController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
+        Route::resource('/carrito', CarritoController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
+        Route::resource('/detalleFactura', DetalleFacturaController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
+    }); //admin prefijo -------------
+
+    
+    Route::post('/user/login', [UserController::class, 'login']);
+  
     //rutas especificas
 
     Route::group(['prefix' => '/client'], function () {
@@ -27,21 +48,21 @@ Route::prefix('v1')->group(function () {
         Route::post('/carrito/{id}/removeProductFromCart', [CarritoController::class, 'removeProductFromCart'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
         Route::post('/carrito/{id}/vaciarCarrito', [CarritoController::class, 'vaciarCarrito'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
         Route::get('/carritos/{id}', [CarritoController::class, 'show'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
-        Route::put('/carritos/{id}', [CarritoController::class, 'update'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
+       // Route::put('/carritos/{id}', [CarritoController::class, 'update'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
         Route::post('/carritos/{id}/productos', 'CarritoController@addProductToCart');
 
         //Usuario
-        Route::post('/user/login', [UserController::class, 'login']);
+        
         Route::get('/user/getidentity', [UserController::class, 'getIdentity'])->middleware(ApiAuthMiddleware::class);
         Route::get('/user/{id}', [UserController::class, 'show'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
         Route::put('/user/{id}', [UserController::class, 'update'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
         Route::post('/user/{id}', [UserController::class, 'store'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
 
-        //Producto
-        Route::post('/producto/upload', [ProductoController::class, 'uploadImage']);
-        Route::get('/producto/getimage/{filename}', [ProductoController::class, 'getImage']);
-        Route::get('/productos/{id}', [ProductoController::class, 'index'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
-        Route::get('/productos/{id}', [ProductoController::class, 'show'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
+           //Producto
+ Route::post('/producto/upload',[ProductoController::class,'uploadImage']);
+   Route::get('/producto/getimage/{filename}',[ProductoController::class,'getImage']);
+   Route::get('/productos/{id}', [ProductoController::class,'index'])->middleware([ApiAuthMiddleware::class,UserMiddleware::class]); 
+   Route::get('/productos/{id}',[ProductoController::class,'show'])->middleware([ApiAuthMiddleware::class,UserMiddleware::class]); 
 
         //compra
         Route::get('/compras', [CompraController::class, 'index'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
@@ -54,15 +75,6 @@ Route::prefix('v1')->group(function () {
         Route::get('/detalle_facturas/{idDetalleFactura}', [DetalleFacturaController::class, 'show'])->middleware([ApiAuthMiddleware::class, UserMiddleware::class]);
     }); //prefijo client --------------
 
-    //rutas automaticas Restful Admin
-    Route::group(['prefix' => '/admin'], function () {
 
-        Route::resource('/compra', CompraController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
-        Route::resource('/user', UserController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]); //Se excluyen porque son obsolutas por temas de seguridad 
-        Route::resource('/bill', BillController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
-        Route::resource('/producto', ProductoController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
-        Route::resource('/carrito', CarritoController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
-        Route::resource('/detalleFactura', DetalleFacturaController::class, ['except' => ['create', 'edit']])->middleware([ApiAuthMiddleware::class, AdminMiddleware::class]);
-    }); //admin prefijo -------------
 
 }); //V1 prefijo ----------------
